b2aca81c98a2d2e918e2e02f25f4d945f3b46736
NieamulKabir <kabir000079@gmail.com>
2024-05-17 11:51:20 +0600

    add middleware

---
+++ middleware.js
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+
+let defaultLocal = "en";
+let locals = ["bn", "en"];
+
+function getLocal(request) {
+  const acceptedLanguage = request.headers.get("accept-language") ?? undefined;
+  let headers = { "accept-language": acceptedLanguage };
+  let languages = new Negotiator({ headers }).languages();
+  return match(languages, locals, defaultLocal);
+}
+
+export function middleware(request) {
+  const pathname = request.nextUrl.pathname;
+
+  const pathnameIsMissingLocal = locals.every(
+    (local) => !pathname.startWith(` /${local}/`) && pathname !== ` /${local}`
+  );
+
+  if (pathnameIsMissingLocal) {
+    const local = getLocal(request);
+
+    return NextResponse.redirect(
+      new URL(` /${local}/${pathname}`, request.url)
+    );
+  }
+}
+export const config = {
+  matcher: [
+    // Skip all internal paths (_next, assets, api)
+    "/((?!api|assets|.*\\..*|_next).*)",
+    // Optional: only run on root (/) URL
+    // '/'
+  ],
+};
